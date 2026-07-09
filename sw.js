c0493dfd61c2d247c157b328e70970f6c08a5fd1
hudsonnestor-cloud <hudsonnestor@hotmail.com>
2026-07-09 12:44:40 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,28 @@
+// Service worker: network-first (sempre busca a versao mais nova quando ha internet;
+// se estiver offline - Farellones, estrada de Pirque - serve a ultima versao em cache)
+const CACHE = 'roteiro-santiago-v2';
+const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    fetch(e.request)
+      .then(resp => {
+        const copy = resp.clone();
+        caches.open(CACHE).then(c => c.put(e.request, copy));
+        return resp;
+      })
+      .catch(() => caches.match(e.request).then(r => r || caches.match('./index.html')))
+  );
+});
